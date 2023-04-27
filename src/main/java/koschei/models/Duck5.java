aa5d04c45ecfc496265ae0f7207104aa99fd75ca
Anatoly M <a3588424@gmail.com>
2023-04-27 06:32:54 +0300

    add spase

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
+
     private final Egg6 egg;
+
     @Autowired
     public Duck5(Egg6 egg) {
         this.egg = egg;
